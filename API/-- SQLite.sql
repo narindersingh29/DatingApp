f08c145930977e173b86847b1033744781d7b0ea
narindersingh29 <narinder80967@gmail.com>
2024-01-11 14:17:13 +0530

    End of sectiongit config --global user.name Y*********

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (id, UserName)
+VALUES (1, "Gurwinder Singh");
+INSERT INTO Users (id, UserName)
+VALUES (2, "Harleen Kaur");
+INSERT INTO Users (id, UserName)
+VALUES (3, "Sukhwinder Singh");
+INSERT INTO Users (id, UserName)
+VALUES (4, "Manjinder Singh");
