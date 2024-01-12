6ae98f428dab063dccc1d610e025b47994947b3d
narindersingh29 <narinder80967@gmail.com>
2024-01-12 10:58:39 +0530

    commit it

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 //Configure the http request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200/"));
 
 app.MapControllers();
 
