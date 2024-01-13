7df72d187076a96c53b4c7afb81dc2c64b0397af
narindersingh29 <narinder80967@gmail.com>
2024-01-13 13:15:57 +0530

    commit

--- API/Controllers/AccountCotroller.cs
+++ API/Controllers/AccountCotroller.cs
@@ -2,7 +2,9 @@
 using System.Text;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -14,19 +16,30 @@ namespace API.Controllers
 {
             _context = context;
         }
-        [HttpPost("register")] //api/account/register?username=dave&password=pwd
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        [HttpPost("register")] // POST: api/account/register?username=dave&password=pwd
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if (await UserExists(registerDto.Username)) return BadRequest("UserName is Taken");
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-             UserName = username,
-             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+             UserName = registerDto.Username.ToLower(),
+             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
              PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+
+        private ActionResult<AppUser> BadRequest(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
